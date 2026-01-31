1a8276d859b81bab34b9fddb7379b3d36946e7c1
Bruno Mulim <bruno.mulim.prog@gmail.com>
2026-01-31 18:17:15 -0300

    feat: reorganize routing structure and improve navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, TouchableOpacity } from "react-native";
-import { TScreenDefinitionsProps } from "../shared/AppRoutes";
+import { TScreenDefinitionsProps } from "../AppRoutes";
 import { Theme } from "../shared/themes/Theme";
 
 export const Home = () => {
   const navigation = useNavigation<TScreenDefinitionsProps>();
   return (
     <View>
-      <Text
-        style={{
-          color: Theme.colors.dark.primary,
-          fontFamily: "NunitoBold",
-          fontSize: 32,
-        }}
-      >
-        Homepage
-      </Text>
-
       <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
-        <Text style={{ fontFamily: "NunitoRegular", fontSize: 24, color: Theme.colors.dark.accent }}>
+        <Text
+          style={{
+            color: Theme.colors.dark.text,
+          }}
+        >
           Configurações
         </Text>
       </TouchableOpacity>
