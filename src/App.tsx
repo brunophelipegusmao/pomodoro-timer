e89aab54c6aa05f601e358165a37f664e37e5d08
Bruno Mulim <bruno.mulim.prog@gmail.com>
2026-01-31 15:43:55 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Text, View } from 'react-native';
+import { Home } from './pages/Home';
+
+export function App() {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
+}
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export const Home = () => {
+  return <View>
+    Homepage
+  </View>;
+};
